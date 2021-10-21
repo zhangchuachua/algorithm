fcf3c75a33967248949c9b4e207715ad2a62c11b
iiiboy <1097341044@qq.com>
2021-10-22 00:51:34 +0800

    快速排序添加解法，还没有写笔记

--- other/排序.ts
+++ other/排序.ts
@@ -1,36 +1,36 @@
-// *快速排序
-export function quickSort(nums: number[]) {
-  if (nums.length === 0 || nums.length === 1) return;
+// *快速排序  在这个排序下遇到了很多的问题。需要好好研究一下下面几个TODO。因为开始没有理解到这些TODO出了很多问题，比如指针指向不对，无法过滤相同的数字等。
+function sortArray(nums: number[]): number[] {
+  function quickSort(arr: number[], start: number = 0, end: number = arr.length - 1) {
+    let l = start;
+    let r = end;
+    if (l >= r) return;
+    const pivot = Math.round(Math.random() * (r - l)) + l;
+    const current = arr[pivot];
+    while (l <= r) { // TODO 为什么需要等于
+      while (arr[l] < current) {
+        l++;
+      }
+      while (arr[r] > current) {
+        r--;
+      }
 
-  function name(arr: number[], start: number, end: number) {
-    if (end >= start) return;
-    // const pivot = Math.floor(Math.random() * (end - start)) + start;
-    const pivot = 0;
-    const current = nums[pivot];
-    let left = start;
-    let right = end;
-    while (left < right) {
-      if (nums[left] > current) {
-        nums[left] = nums[left] ^ nums[right];
-        nums[right] = nums[left] ^ nums[right];
-        nums[left] = nums[left] ^ nums[right];
-        right--;
-      } else if (nums[right] < current) {
-        nums[left] = nums[left] ^ nums[right];
-        nums[right] = nums[left] ^ nums[right];
-        nums[left] = nums[left] ^ nums[right];
-        left++;
-      } else
-        left++;
+      if (l <= r) { // TODO 为什么需要等于
+        arr[l] = arr[l] ^ arr[r]; // TODO 使用位运算会出错
+        arr[r] = arr[l] ^ arr[r];
+        arr[l] = arr[l] ^ arr[r];
+        l++;
+        r--;
+      }
+      console.log(pivot, l, r, arr);
     }
-    nums[left] = current;
-    console.log(nums, pivot, start, end);
-    name(arr, start, left - 1);
-    name(arr, left + 1, end);
+    quickSort(arr, start, l - 1);
+    quickSort(arr, l, end);
   }
 
-  name(nums, 0, nums.length - 1);
-  console.log(nums);
-}
+  quickSort(nums);
+  return nums;
+};
+console.log(sortArray([5, 2, 3, 1]));
 
-quickSort([19, 97, 9, 17, 1]);
+
+export default sortArray;

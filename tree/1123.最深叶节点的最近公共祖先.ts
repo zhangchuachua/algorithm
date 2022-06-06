1a5bd9d2ca845655ef7d4b1e3886985db8b38d7c
iiiboy <1097341044@qq.com>
2022-06-07 01:58:42 +0800

    feat: 完成 1123.最深叶节点的最近公共祖先.ts 使用的方法比较简陋，可以进一步优化

--- tree/1123.最深叶节点的最近公共祖先.ts
+++ tree/1123.最深叶节点的最近公共祖先.ts
@@ -1,41 +1,53 @@
 // *leetcode-145周赛-第二题 1123. 最深叶节点的最近公共祖先 medium
-import TreeNode from "./TreeNode";
+import TreeNode, { createTree } from "./TreeNode";
 
 interface LeafMessage {
-    self: TreeNode,
-    depth: number,
-    parent: number | null;
+  self: TreeNode,
+  depth: number,
+  parent: number | null;
 }
 
 function lcaDeepestLeaves(root: TreeNode | null): TreeNode | null {
-    if (!root) return null;
-    if (!root.left && !root.right) return root;
-    const map = new Map<number, LeafMessage>();
-    let maxDeep = 0;
-    function dfs(r: TreeNode, depth: number, parent: TreeNode | null) {
-        if (!map.has(r.val)) map.set(r.val, {
-            self: r,
-            depth,
-            parent: parent ? parent.val : null
-        })
-        maxDeep = depth > maxDeep ? depth : maxDeep;
+  if (!root) return null;
+  if (!root.left && !root.right) return root;
+  const map = new Map<number, LeafMessage>();
+  let maxDeep = 0;
 
-        if (r.left) dfs(r.left, depth + 1, r);
-        if (r.right) dfs(r.right, depth + 1, r);
+  function dfs(r: TreeNode, depth: number, parent: TreeNode | null) {
+    if (!map.has(r.val)) map.set(r.val, {
+      self: r,
+      depth,
+      parent: parent ? parent.val : null
+    })
+    maxDeep = depth > maxDeep ? depth : maxDeep;
+
+    if (r.left) dfs(r.left, depth + 1, r);
+    if (r.right) dfs(r.right, depth + 1, r);
+  }
+
+  dfs(root, 1, null);
+  const deepestLeaf: LeafMessage[] = [];
+  for (let i of map.values()) {
+    if (i.depth >= maxDeep) {
+      deepestLeaf.push(i);
     }
+  }
 
-    dfs(root, 1, null);
-
-    const deepestLeaf = [];
-    for(let {depth} of map.values()) {
-        if(depth >= maxDeep) {
-            deepestLeaf.push({
-
-            })
+  function getCommonParent(args: LeafMessage[]): TreeNode {
+    if (args.length === 1) return args[0].self;
+    const parentList: LeafMessage[] = []
+    args.forEach(({ parent }) => {
+        if (parent && map.has(parent)) {
+          parentList.push(map.get(parent) as LeafMessage);
+          map.delete(parent);
         }
-    }
+      }
+    )
+    return getCommonParent(parentList);
+  }
 
-    if (deepestLeaf.length === 1) return new TreeNode(deepestLeaf[0].val);
-
-
+  return getCommonParent(deepestLeaf);
 }
+
+const tree = createTree([3, 5, 1, 6, 2, 0, 8, null, null, 7, 4]);
+console.log(lcaDeepestLeaves(tree))
--- tree/TreeNode.ts
+++ tree/TreeNode.ts
@@ -8,4 +8,27 @@ export default class TreeNode {
     this.left = left ?? null;
     this.right = right ?? null;
   }
+
+}
+
+export function createTree(args: (number | null)[]): TreeNode | null {
+  if (!args || args[0] === null) return null;
+  const root = new TreeNode(args.shift() as number);
+  const queue = [root];
+  while (queue.length && args.length) {
+    const node = queue.shift() as TreeNode;
+    const v1 = args.shift();
+    const v2 = args.shift();
+    if (v1 !== null) {
+      const left = new TreeNode(v1)
+      node.left = left
+      queue.push(left)
+    }
+    if (v2 !== null) {
+      const right = new TreeNode(v2);
+      node.right = right;
+      queue.push(right);
+    }
+  }
+  return root;
 }
